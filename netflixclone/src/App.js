bd62de1c75ded2e3726f8c6700d3600fe0073b01
Edio Mello <e.mello.dev@gmail.com>
2022-08-08 00:15:58 -0400

    Add Content - NetflixClone Project

---
+++ netflixclone/src/App.js
@@ -0,0 +1,66 @@
+import React, { useEffect } from 'react';
+import Tmdb from './Tmdb';
+import './App.css';
+import MovieRow from './components/MovieRow';
+import FeatureMovie from './components/FeaturedMovie';
+import Header from './components/Header/';
+
+export default () => {
+
+    const [movieList, setMovieList] = React.useState([]);
+    const [featureData, setFeatureData] = React.useState(null);
+    const [blackHeader, setBlackHeader] = React.useState(false);
+
+    useEffect(() => {
+        const loadAll = async () => {
+            //Pegar a lista totaal
+            const list = await Tmdb.getHomeList();
+            setMovieList(list);
+
+            //Pegando o Feature
+            let originals = list.filter(i=> i.slug === 'originals');
+            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+            let chosen = originals[0].items.results[randomChosen];
+            let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+            setFeatureData(chosenInfo);
+        }
+
+        loadAll();
+    }, []);
+
+    useEffect(() => {
+        const scrollListener = () => {
+            if(window.scrollY > 100) {
+                setBlackHeader(true);
+            } else {
+                setBlackHeader(false);
+            }
+        }
+
+        window.addEventListener('scroll', scrollListener);
+        return () => {
+            window.removeEventListener('scroll', scrollListener);
+        }
+    }, []);
+
+  return (
+    <div className="page">
+
+        <Header black={blackHeader}/>
+        
+        {featureData && <FeatureMovie item={featureData}/>}
+
+         <section className="lists">
+            {movieList.map((item, key) => (
+                <MovieRow key={key} title={item.title} items={item.items}/>
+            ))}
+        </section>
+
+        <footer>
+            Feito com <span role="img" aria-label="Heart">❤️</span><br/>
+            Direitos de imagem para Netflix<br/>
+            Dados pego do site Themoviedb.org<br/>
+        </footer>
+    </div>
+  );
+}
---
+++ netflixclone/src/components/MovieRow.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './MovieRow.css';
+import {NavigateBeforeIcon} from '@mui/icons-material';
+import {NavigateNextIcon} from '@mui/icons-material';
+
+export default ({title, items}) => {
+    return (
+        <div>
+            <div className="movie-row">
+                <h2>{title}</h2>
+                <div className="">
+
+                </div>
+            </div>
+
+            <div className="movieRow--listarea">
+                <div className="movieRow--list">
+                    {items.results.length > 0 && items.results.map((item, key) => (
+                        <div key={key} className="movieRow--item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
