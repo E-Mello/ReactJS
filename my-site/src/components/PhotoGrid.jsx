ea1f8ada8fc0e808829ff8c1da755b47aa9f9731
Mello <e.mello.dev@gmail.com>
2022-08-18 23:17:51 -0400

    Fix Content

--- my-site/src/components/PhotoGrid.jsx
+++ my-site/src/components/PhotoGrid.jsx
@@ -4,10 +4,29 @@ import { useState } from 'react';
 export function PhotoGrid() {
     
     function handleStateVideo() {
-        
-    }
-    
-    const x = document.getElementById('x');
+        const [stateVideo, setStateVideo] = useState(0);
+        // <video> is used for top-level audio documents as well
+        let videoElement = document.getElementsByTagName("video")[0];
+      
+        const setFocusToVideoElement = (e) => {
+          // We don't want to retarget focus if it goes to the controls in
+          // the video element. Because they're anonymous content, the target
+          // will be the video element in that case. Avoid calling .focus()
+          // for those events:
+          if (e && e.target == videoElement) {
+            return;
+          }
+          videoElement.focus();            
+        };
+      
+        // Redirect focus to the video element whenever the document receives
+        // focus.
+        document.addEventListener("play", setFocusToVideoElement, true);
+      
+        // Focus on the video in the newly created document.
+        setFocusToVideoElement();
+                     
+  } 
 
     return (
         <section className={styles.section02}>
@@ -21,9 +40,9 @@ export function PhotoGrid() {
             <div className={styles.photoGrid}>
                 <div className={styles.photoGridList}>
                     <div className={styles.photoGridItem}>
-                        <video src="../public/Darling.mp4" loop className={handleStateVideo}></video>
-                        <video src="../public/Darling.mp4" loop className={handleStateVideo}></video>
-                        <video src="../public/Darling.mp4" loop className={handleStateVideo}></video>
+                        <video src="../public/Darling.mp4" onMouseOver={handleStateVideo}></video>
+                        <video src="../public/Darling.mp4" onMouseOver={handleStateVideo}></video>
+                        <video src="../public/Darling.mp4" onMouseOver={handleStateVideo}></video>
                     </div>
                 </div>        
             </div>
