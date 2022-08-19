e088014ac5ea7f231a24f02effb754fc7a772221
Edio Mello <e.mello.dev@gmail.com>
2022-08-19 00:10:17 -0400

    Add Finally The Video

--- my-site/src/components/PhotoGrid.jsx
+++ my-site/src/components/PhotoGrid.jsx
@@ -3,30 +3,15 @@ import { useState } from 'react';
 
 export function PhotoGrid() {
     
-    function handleStateVideo() {
-        const [stateVideo, setStateVideo] = useState(0);
-        // <video> is used for top-level audio documents as well
-        let videoElement = document.getElementsByTagName("video")[0];
-      
-        const setFocusToVideoElement = (e) => {
-          // We don't want to retarget focus if it goes to the controls in
-          // the video element. Because they're anonymous content, the target
-          // will be the video element in that case. Avoid calling .focus()
-          // for those events:
-          if (e && e.target == videoElement) {
-            return;
-          }
-          videoElement.focus();            
-        };
-      
-        // Redirect focus to the video element whenever the document receives
-        // focus.
-        document.addEventListener("play", setFocusToVideoElement, true);
-      
-        // Focus on the video in the newly created document.
-        setFocusToVideoElement();
-                     
-  } 
+    const stopMovie = (e) => {
+    e.target.pause();
+    // console.log('off');
+  }
+  
+  const playMovie = (e) => {
+    e.target.play();
+    // console.log('on');
+  }
 
     return (
         <section className={styles.section02}>
@@ -40,9 +25,12 @@ export function PhotoGrid() {
             <div className={styles.photoGrid}>
                 <div className={styles.photoGridList}>
                     <div className={styles.photoGridItem}>
-                        <video src="../public/Darling.mp4" onMouseOver={handleStateVideo}></video>
-                        <video src="../public/Darling.mp4" onMouseOver={handleStateVideo}></video>
-                        <video src="../public/Darling.mp4" onMouseOver={handleStateVideo}></video>
+                        <video poster="../public/love-heart-dark.jpg" src="../public/Darling.mp4" onMouseOver={playMovie} onMouseOut={stopMovie} preload='none' loop/>
+                        <video poster="../public/love-heart-dark.jpg" src="../public/Darling.mp4" onMouseOver={playMovie} onMouseOut={stopMovie} preload='none' loop/>
+                    </div>
+                    <div className={styles.photoGridItem}>
+                        <video poster="../public/love-heart-dark.jpg" src="../public/Darling.mp4" onMouseOver={playMovie} onMouseOut={stopMovie} preload='none' loop/>
+                        <video poster="../public/love-heart-dark.jpg" src="../public/Darling.mp4" onMouseOver={playMovie} onMouseOut={stopMovie} preload='none' loop/>
                     </div>
                 </div>        
             </div>
