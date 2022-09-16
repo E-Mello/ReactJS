70957de9ab9ba0adcee17ec06132aa04487c7932
Mello <e.mello.dev@gmail.com>
2022-09-16 19:59:57 -0400

    Add adjust in icons

--- WebSite-Unemat/src/components/SideNavBarLeft.tsx
+++ WebSite-Unemat/src/components/SideNavBarLeft.tsx
@@ -1,23 +1,37 @@
 import { Avatar } from "./Avatar";
 import { LogoUnemat } from "./LogoUnemat";
-import { BsFillArrowLeftSquareFill, BsSearch } from "react-icons/bs";
+import { BsFillArrowLeftSquareFill, BsSearch, BsFillArrowRightSquareFill } from "react-icons/bs";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 import styles from './SideNavBarLeft.module.css'
 
 
 
 
+
 export function SideNavBarLeft() {
+    const [isActive, setIsActive] = useState(true);
+
+    function changeStateBtn() {
+        setIsActive(!isActive);
+        console.log(isActive);
+
+    }
+
     return (
         <>
-            <div className={styles.sideMenu}>
+            <div className={isActive ? styles.sideMenuActive : styles.sideMenuInactive}>
                 <div className={styles.topSection}>
                     <div className={styles.logo}>
                         <LogoUnemat src="../src/assets/LogoUnemat.png" />
                     </div>
-                    <div className={styles.toggleMenuBtn}>
-                        <BsFillArrowLeftSquareFill className={styles.arrowLeft} size={20} />
+                    <div className={styles.toggleMenuBtn} onClick={changeStateBtn}>
+                        {isActive ?
+                            <BsFillArrowLeftSquareFill className={styles.arrowLeft} size={20} style={{ transitionDelay: '0.3s' }} />
+                            :
+                            <BsFillArrowRightSquareFill className={styles.arrowRight} size={20} />
+                        }
                     </div>
                 </div>
 
